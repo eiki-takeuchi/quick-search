84efadf97e45bf9b1a5f4c99baeec74ed9d90e45
eitake0002 <eiki.e.t.d@gmail.com>
2017-10-31 15:51:47 +0900

    Remove class

--- quick-search.py
+++ quick-search.py
@@ -9,159 +9,152 @@ import sys
 import traceback
 from pprint import pprint
 import re
-import colorama
-from colorama import Fore, Back, Style
 import pydoc
 from fabric.colors import green
 from fabric.colors import red
+from fabric.colors import cyan
 import yaml
 
-colorama.init(autoreset=True)
 
+SEARCH_ENGINE_ID=os.environ["SEARCH_ENGINE_ID"]
+GOOGLE_API_KEY=os.environ["GOOGLE_API_KEY"]
 
-class GoogleSearch():
+def search(query, start=1, num=10):
+    """Get title, link list.
 
-    def __init__(self, num=5, loop_num=3, tmp_stop=5):
-        self.num = num
-        self.loop_num = loop_num
-        self.tmp_stop = tmp_stop
-        self.SEARCH_ENGINE_ID = os.environ["SEARCH_ENGINE_ID"]
-        self.GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+    Execute google search api format results to specified number of list.
 
-    def search(self, query, start=1, num=10):
-        """Get title, link list.
+    Ex: 
+    search("Hello")
 
-        Execute google search api format results to specified number of list.
+    Parameter
+    ---------
+    query : str
+        Search query.
 
-        Ex: 
-        search("Hello")
+    start : int
+        Number of page to start.
 
-        Parameter
-        ---------
-        query : str
-            Search query.
+    num : int
+        Number of resutls to get.
 
-        start : int
-            Number of page to start.
+    Return
+    ------
+    list : 
+        Title and link list.
 
-        num : int
-            Number of resutls to get.
+    """
 
-        Return
-        ------
-        list : 
-            Title and link list.
-
-        """
-
-        title_links = []
-        for start_num in range(3):
-            start_num += 1
-            api_path = "https://www.googleapis.com/customsearch/v1"
-            params = {
-                "cx": self.SEARCH_ENGINE_ID,
-                "key": self.GOOGLE_API_KEY,
-                "q": query,
-                "start": start_num,
-                "num": 10
-            }
-
-            try:
-                result_json = requests.get(api_path, params).json()
-                items_json = result_json["items"]
-
-                for v in items_json:
-                    title_link = [v["title"], v["link"]]
-                    title_links.append(title_link)
-
-            except:
-                pprint(result_json)
-                traceback.print_exc()
-
-        return title_links[:num]
-
-    def contents_scraping(self, link, remove_space=True, remove_lb=True):
-        """
-        Scraping contents.
-
-        Parameter
-        ---------
-        url : str
-          Scraping target url.
-
-        Return
-        ------
-        list : 
-            title and contents.
-        """
-
-        try:
-            html = urllib.request.urlopen(link).read()
-        except:
-            print("ERROR : failed to get contents.")
-            return (False, "")
-
-        title = Document(html).short_title()
-        contents = Document(html).summary()
-        contents = html2text.html2text(contents)
-
-        p = re.compile(r"<[^>]*?>")
-        c = p.sub("", contents)
-
-        if remove_space is True:
-            c = c.replace(" ", "")
-
-        if remove_lb is True:
-            c = c.replace("\r", "")
-            c = c.replace("\n", "")
-
-        return title, c
-
-    def search_images(self, word, num=10):
-        """
-        Get Images from google search.
-
-        Ex: 
-          search_images("cat", 5)
-
-        Parameter
-        ---------
-        word : str
-          Search word.
-
-        num : int
-          Number of images to get.
-
-        Return
-        ------
-        list : list
-          Image urls.
-
-        ["https://example.jpg", "https://example2.png"]
-        """
-
+    title_links = []
+    for start_num in range(3):
+        start_num += 1
         api_path = "https://www.googleapis.com/customsearch/v1"
         params = {
-            "cx": "001155821352449308988:0skw1nrswlm",
-            "key": "AIzaSyDXu5ftNYEHOiq8wwUnSBbB4lnshkn1Syw",
-            "q": word,
-            "searchType": "image",
-            "start": 1,
+            "cx": SEARCH_ENGINE_ID,
+            "key": GOOGLE_API_KEY,
+            "q": query,
+            "start": start_num,
             "num": 10
         }
 
         try:
             result_json = requests.get(api_path, params).json()
             items_json = result_json["items"]
+
+            for v in items_json:
+                title_link = [v["title"], v["link"]]
+                title_links.append(title_link)
+
         except:
             pprint(result_json)
+            traceback.print_exc()
 
-        image_links = []
-        for i, v in enumerate(items_json):
-            if i < num:
-                image_links.append(v["link"])
+    return title_links[:num]
 
-        return image_links
+
+def contents_scraping(link, remove_space=True, remove_lb=True):
+    """
+    Scraping contents.
+
+    Parameter
+    ---------
+    url : str
+      Scraping target url.
+
+    Return
+    ------
+    list : 
+        title and contents.
+    """
+
+    try:
+        html = urllib.request.urlopen(link).read()
+    except:
+        print("ERROR : failed to get contents.")
+        return (False, "")
+
+    title = Document(html).short_title()
+    contents = Document(html).summary()
+    contents = html2text.html2text(contents)
+
+    p = re.compile(r"<[^>]*?>")
+    c = p.sub("", contents)
+
+    if remove_space is True:
+        c = c.replace(" ", "")
+
+    if remove_lb is True:
+        c = c.replace("\r", "")
+        c = c.replace("\n", "")
+
+    return title, c
+
+
+def search_images(word, num=10):
+    """
+    Get Images from google search.
+
+    Ex: 
+      search_images("cat", 5)
+
+    Parameter
+    ---------
+    word : str
+      Search word.
+
+    num : int
+      Number of images to get.
+
+    Return
+    ------
+    list : list
+      Image urls.
+
+    ["https://example.jpg", "https://example2.png"]
+    """
+
+    api_path = "https://www.googleapis.com/customsearch/v1"
+    params = {
+        "cx": "001155821352449308988:0skw1nrswlm",
+        "key": "AIzaSyDXu5ftNYEHOiq8wwUnSBbB4lnshkn1Syw",
+        "q": word,
+        "searchType": "image",
+        "start": 1,
+        "num": 10
+    }
+
+    try:
+        result_json = requests.get(api_path, params).json()
+        items_json = result_json["items"]
+    except:
+        pprint(result_json)
+
+    image_links = []
+    for i, v in enumerate(items_json):
+        if i < num:
+            image_links.append(v["link"])
+
+    return image_links
 
 
 if __name__ == '__main__':
@@ -192,18 +185,17 @@ if __name__ == '__main__':
             query += " " + q
 
     # Execute google search.
-    gs = GoogleSearch()
-    title_links = gs.search(query, num=RESULT_NUM)
+    title_links = search(query, num=RESULT_NUM)
 
     for i, title_link in enumerate(title_links):
 
         # Print stdout.
         i += 1
-        print(Fore.RED + str(i) + " ", end='')
-        print(Fore.GREEN + title_link[0])
-        print("  " + Fore.CYAN + title_link[1])
+        print(red(str(i)) + " ", end='')
+        print(green(title_link[0]))
+        print("  " + cyan(title_link[1]))
 
-        title, contents = gs.contents_scraping(title_link[1])
+        title, contents = contents_scraping(title_link[1])
         if title is False:
             next
 
@@ -238,7 +230,7 @@ if __name__ == '__main__':
             # Show in terminal.
             elif page_num.isdigit() is True:
                 link = title_links[int(page_num)][1]
-                title, contents = gs.contents_scraping(link, False, False)
+                title, contents = contents_scraping(link, False, False)
                 if title is False:
                     print("ERROR : failed to scrape contents.")
                     next
